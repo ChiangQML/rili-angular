f080f349dcf798e04a88a2f19c94c32629c450ef
ChiangQML <77223784+ChiangQML@users.noreply.github.com>
2024-02-26 12:15:28 -0800

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rili-angular';
+ 
 }
+
+  // public event_name = "Board Meeting";
+  // public start_time = "8:00am";
+  // public end_time = "3:00pm";
+  // public start_date = "04/01/2024";
+  // public end_date = "04/07/2024";
+  // public description = "Discuss next steps for acquisition between board members and executives";
+  // public location = "Norton Building, Suite 201";
