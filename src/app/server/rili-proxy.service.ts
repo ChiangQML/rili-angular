1b4401594957695628415adb01656d05b0cd72d6
ChiangQML <77223784+ChiangQML@users.noreply.github.com>
2024-02-27 03:33:10 -0800

    Update rili-proxy.service.ts

--- src/app/server/rili-proxy.service.ts
+++ src/app/server/rili-proxy.service.ts
@@ -5,21 +5,17 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class RiliProxyService {
-  // where to get database data from
   hostUrl = 'http://localhost:8080';
 
-  // xml data to grab data from api
   constructor(private httpClient: HttpClient) { }
 
-  // methods below to grab content from database
+  events:any=[]
 
-  post_event(eventData: any){
-    return this.httpClient.post<any>(this.hostUrl,eventData);
+  post_event(eventData: any) {
+    return this.httpClient.post<any>(`${this.hostUrl}/events`, eventData);
   }
-  
-  // get all events
+
   getEvents() {
-    return this.httpClient.get<any[]>( this.hostUrl + '/app/rili');
+    return this.httpClient.get<any[]>(`${this.hostUrl}/all-events`);
   }
-
 }
